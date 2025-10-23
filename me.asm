96cd23ec7d6f9f2a67e4096cb4a600e7ebc2181d
Houndzkie <helge.justine.dano@gmail.com>
2025-10-23 22:28:27 +0800

    good shit

---
+++ me.asm
@@ -0,0 +1,60 @@
+3 memory module
+4 unit of transfer
+5 performance
+6 Basic Instruction Cycle
+7 Data operation
+8 IAS Memory Formats
+9 instruction cycle
+10 Possible Organization of a Simple Computer System
+11 pipelining
+12 cache
+13 DRAM
+14 Program Flow of Control without and with Interrupts
+15 PCI
+16 Instruction Cycle State Diagram
+17 Characteristics of Internal Memory Devices in a Memory Architecture
+18 clock rate
+19 location
+20 Memory cycle time
+21 instruction fetch
+22 Simplified View of Major Elements of a Multicore Computer
+23 temporal locality
+24 clock
+25 gate
+26 L1
+27 ISA
+28 principle of locality
+29 superscalar execution
+30 Data Locality of Reference for Web-Based Document Access Application
+31 speculative execution
+32 External memory
+33 program counter
+34 latency
+35 Typical I/O Device Data Rates
+36 hardware and software approaches
+37 Categories of Memory Faults
+38 word
+39 bandwidth
+40 The Memory Hierarchy
+41 data flow analysis
+42 Static instruction
+43 IoT
+44 Amdahl’s law
+45 bytes
+46 MCM
+47 Key Characteristics of Computer Memory Systems
+48 processor
+49 interrupt handler
+50 latency
+51 capacity, access time, and cost
+52 Growth in Transistor Count on Integrated Circuits
+53 Multichip Module
+54 instruction
+55 fetch
+56 speculative execution
+57 IBM z13 Memory Hierarchy
+58 hardware and software approaches
+59 The Computer: Top-Level Structure
+60 execute
+61 I/O module
+62 von Neumann machines
